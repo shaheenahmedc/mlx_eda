17c175097e56adc483cc197d52a01e27bca9b17e
Shaheen Ahmed-Chowdhury <shaheen.ahmed@live.co.uk>
2023-12-07 16:05:05 +0100

    add 4 ddpm files

---
+++ week_7/ddpm.py
@@ -0,0 +1,81 @@
+import os
+import torch
+import torch.nn as nn
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from torch import optim
+import logging
+from torch.utils.tensorboard import SummaryWriter
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+class Diffusion:
+    def __init__(
+        self,
+        noise_steps=1000,
+        beta_start=1e-4,
+        beta_end=0.02,
+        img_size=64,
+        device="cuda",
+    ) -> None:
+        self.noise_steps = noise_steps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.img_size = img_size
+        self.device = device
+
+        self.beta = self.prepare_noise_schedule().to(self.device)
+        self.alpha = 1.0 - self.beta
+        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+
+    def prepare_noise_schedule(self):
+        betas = torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
+        return betas
+
+    def noise_images(self, x, t):
+        # Expand dimension of alphas for later
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1.0 - self.alpha_hat[t])[
+            :, None, None, None
+        ]
+        eps = torch.randn_like(x)
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * eps, eps
+
+    def sample_timesteps(self, n):
+        return torch.randint(low=1, high=self.noise_steps, size=(n,))
+
+    def sample(self, model, n):
+        logging.info(f"Sampling {n} new images")
+        model.eval()
+        with torch.no_grad():
+            x = torch.randn(n, 3, self.img_size, self.img_size).to(self.device)
+            for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
+                t = (torch.ones(n) * i).long().to(self.device)
+                predicted_noise = model(x, t)
+                alpha = self.alpha[t][:, None, None, None]
+                alpha_hat = self.alpha_hat[t][:, None, None, None]
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    noise = torch.randn_like(x)
+                else:
+                    # Don't need noise for last timestep
+                    noise = torch.zeros_like(x)
+                # TODO: Format below better/flake8 it
+                x = (
+                    1
+                    / torch.sqrt(alpha)
+                    * (
+                        x
+                        - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise
+                    )
+                    + torch.sqrt(beta) * noise
+                )
+                model.train()
+                x = (x.clamp(-1, 1) + 1) / 2
+                x = x * 255
+                return x
